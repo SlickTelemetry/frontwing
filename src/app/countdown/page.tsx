b1f540d6ab56b72e57ffffc5a8716cbfbd3908e9
cris lombardo <bigc4997@gmail.com>
2026-01-03 09:57:19 -0500

    chore: countdown page to 2026

--- src/app/countdown/page.tsx
+++ src/app/countdown/page.tsx
@@ -2,76 +2,88 @@
 
 import { useEffect, useState } from 'react';
 
-const date = '2025-03-13T21:30:00-04:00';
-const breakdownDifference = (targetDate: Date, now: Date) => {
+import { LandingNav } from '@/app/_components/nav';
+
+const date = '2026-03-06T12:00:00';
+const gapToDate = (targetDate: Date) => {
+  const now = new Date();
+  const timeDiff = targetDate.getTime() - now.getTime();
+  if (timeDiff <= 0) {
+    return {
+      months: 0,
+      days: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
+  const totalMonths =
+    targetDate.getMonth() -
+    now.getMonth() +
+    12 * (targetDate.getFullYear() - now.getFullYear()); // Get overlap into next year
+
   return {
-    months:
-      targetDate.getMonth() -
-      now.getMonth() +
-      12 * (targetDate.getFullYear() - now.getFullYear()),
-    weeks: Math.floor(
-      (targetDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24 * 7),
-    ),
-    days:
-      Math.floor(
-        (targetDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24),
-      ) % 7,
-    minutes:
-      Math.floor((targetDate.getTime() - now.getTime()) / (1000 * 60)) % 60,
-    seconds: Math.floor((targetDate.getTime() - now.getTime()) / 1000) % 60,
+    months: totalMonths,
+    days: Math.floor(timeDiff / (1000 * 60 * 60 * 24)) % 7,
+    minutes: Math.floor(timeDiff / (1000 * 60)) % 60,
+    seconds: Math.floor(timeDiff / 1000) % 60,
   };
 };
 export default function CountdownPage() {
-  const [duration, setDuration] = useState(
-    breakdownDifference(new Date(date), new Date()),
-  );
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const diff = breakdownDifference(new Date(date), new Date());
-      setDuration(diff);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <main className='h-[700px] bg-[url(https://upload.wikimedia.org/wikipedia/commons/8/8e/Melbourne_Grand_Prix_Circuit%2C_March_22%2C_2018_SkySat_%28cropped%29.jpg)] bg-cover bg-center text-center'>
-      <div className='flex h-full w-full flex-col items-center justify-center gap-8 bg-black/60'>
-        <h1 className='text-4xl font-bold md:text-6xl'>Countdown to 2025</h1>
-        <div className='grid grid-cols-5 gap-2'>
-          <Digit time={duration.months} name='month' />
-          <Digit time={duration.weeks} name='week' />
-          <Digit time={duration.days} name='day' />
-          <Digit time={duration.minutes} name='minute' />
-          <Digit time={duration.seconds} name='second' />
-        </div>
-        <div>
-          <h2 className='text-2xl md:text-4xl'>
-            Practice 1<br />
-            Australian Grand Prix
-          </h2>
-          <p>
-            {new Date(date).toLocaleString(undefined, {
-              weekday: 'long',
-              month: 'long',
-              day: 'numeric',
-              year: 'numeric',
-              hour: 'numeric',
-              minute: 'numeric',
-              hour12: true,
-            })}
-          </p>
+    <main className='h-screen bg-[url(https://upload.wikimedia.org/wikipedia/commons/8/8e/Melbourne_Grand_Prix_Circuit%2C_March_22%2C_2018_SkySat_%28cropped%29.jpg)] bg-cover bg-center text-center'>
+      <div className='bg-background/60 h-screen flex-1'>
+        <div className='flex h-full flex-col items-center'>
+          <LandingNav />
+          <div className='flex w-full flex-1 flex-col items-center justify-center gap-8'>
+            <h1 className='text-4xl font-bold md:text-6xl xl:text-8xl'>
+              Countdown to 2026
+            </h1>
+            <div>
+              <p className='text-2xl md:text-4xl'>
+                Practice 1 of the Australian Grand Prix
+              </p>
+              <p>
+                {new Date(date).toLocaleString(undefined, {
+                  weekday: 'long',
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })}
+              </p>
+            </div>
+            <Countdown />
+          </div>
         </div>
       </div>
     </main>
   );
 }
 
+function Countdown() {
+  const [duration, setDuration] = useState(gapToDate(new Date(date)));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const diff = gapToDate(new Date(date));
+      setDuration(diff);
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className='grid grid-cols-4 gap-2'>
+      <Digit time={duration.months} name='month' />
+      <Digit time={duration.days} name='day' />
+      <Digit time={duration.minutes} name='minute' />
+      <Digit time={duration.seconds} name='second' />
+    </div>
+  );
+}
+
 const Digit = ({ time, name }: { time: number; name: string }) => {
   return (
     <div className='bg-background/75 grid w-32 rounded-xl py-2 text-center'>
-      <p className='text-2xl md:text-6xl xl:text-8xl'>{time}</p>
+      <p className='text-2xl md:text-6xl'>{time}</p>
       <p className='lg:text-2xl'>
         {name}
         {time !== 1 && 's'}
