c75b303dd41da3e5bacf66b5ad1b7c6bfb107229
cris lombardo <bigc4997@gmail.com>
2026-02-01 00:20:59 -0500

    feat: FRON-242 telemetry charts

---
+++ src/app/[year]/[event]/[session]/telemetry/page.tsx
@@ -0,0 +1,62 @@
+import { notFound } from 'next/navigation';
+
+import { getNextEvent } from '@/features/telemetry/api/get-telemetry';
+import { BrakeChart } from '@/features/telemetry/components/brake-chart';
+import { DRSChart } from '@/features/telemetry/components/drs-chart';
+import { GearChart } from '@/features/telemetry/components/gear-chart';
+import { RPMChart } from '@/features/telemetry/components/rpm-chart';
+import { SpeedChart } from '@/features/telemetry/components/speed-chart';
+import { ThrottleChart } from '@/features/telemetry/components/throttle-chart';
+
+async function TelemetryExample() {
+  const { data, error } = await getNextEvent();
+
+  if (!data || error) return notFound();
+
+  // const driverSessions = data.driver_sessions.map((ds) => {
+  //   return {
+  //     ...ds,
+  //     telemetries: ds.telemetries.map((dataPoint) => ({
+  //       ...dataPoint,
+  //       session_time: parseInt(
+  //         dataPoint.session_time?.toString().slice(0, 6) || '',
+  //       ),
+  //     })),
+  //   };
+  // });
+
+  return (
+    <div className='p-4'>
+      <div className='flex gap-2'>
+        <div>Select Year</div>
+        <div>Select Event</div>
+        <div>Select Session</div>
+        <div>Select Driver</div>
+        <div>Select Lap</div>
+      </div>
+
+      <div>
+        <div className='h-80'>
+          <SpeedChart driverSessions={data.driver_sessions} />
+        </div>
+        <div className='h-80'>
+          <RPMChart driverSessions={data.driver_sessions} />
+        </div>
+        <div className='h-80'>
+          <ThrottleChart driverSessions={data.driver_sessions} />
+        </div>
+        <div className='h-80'>
+          <GearChart driverSessions={data.driver_sessions} />
+        </div>
+        <div className='h-80'>
+          <BrakeChart driverSessions={data.driver_sessions} />
+        </div>
+        <div className='h-80'>
+          <DRSChart driverSessions={data.driver_sessions} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TelemetryExample;
