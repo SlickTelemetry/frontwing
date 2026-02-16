6f35a1f7f44a58241ae34e0e55ba2f1f6a3a7f9b
Pratik Borole <borolepratik@gmail.com>
2026-02-16 19:58:43 +0530

    chore: first stab at watermarking the charts
    need a few things fixed first; amend this commit when ready

--- src/hooks/use-EChart.tsx
+++ src/hooks/use-EChart.tsx
@@ -1,6 +1,7 @@
 import { BarChart, LineChart, ScatterChart } from 'echarts/charts';
 import {
   DataZoomComponent,
+  GraphicComponent,
   GridComponent,
   TooltipComponent,
 } from 'echarts/components';
@@ -16,6 +17,7 @@ echarts.use([
   LineChart,
   DataZoomComponent,
   TooltipComponent,
+  GraphicComponent,
   GridComponent,
   CanvasRenderer,
 ]);
@@ -26,6 +28,28 @@ export function useECharts(ref: React.RefObject<HTMLDivElement | null>) {
   useEffect(() => {
     if (!ref.current || chartInstance.current) return;
     chartInstance.current = echarts.init(ref.current);
+
+    // TODO!: figure out why standings and lap times chart wont show this
+    chartInstance.current.setOption({
+      graphic: {
+        elements: [
+          {
+            type: 'text',
+            left: 'center',
+            top: 'center',
+            z: 100,
+            rotation: Math.PI / 4,
+            style: {
+              text: 'Slick Telemetry',
+              font: 'bold 26px sans-serif', // TODO!: find a way to increase the size of the text WITHOUT affecting the chart UI
+              fill: '#fff', // TODO!: add support for dark and light mode
+              opacity: 0.5,
+            },
+          },
+        ],
+      },
+    });
+
     return () => {
       chartInstance.current?.dispose();
       chartInstance.current = null;
